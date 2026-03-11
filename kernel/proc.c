6d1608d58d4ad5121128786839ba2fc8c1379b03
doge316 <2281751933@qq.com>
2026-03-12 00:38:25 +0800

    lab3 加速系统调用

--- kernel/proc.c
+++ kernel/proc.c
@@ -6,6 +6,10 @@
 #include "proc.h"
 #include "defs.h"
 
+struct usyscall{
+    int pid;
+};
+
 struct cpu cpus[NCPU];
 
 struct proc proc[NPROC];
@@ -132,6 +136,14 @@ found:
     return 0;
   }
 
+  //初始化usyscall，设置pid
+  if((p->usyscall=(struct usyscall*)kalloc())==0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid=p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -158,6 +170,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->usyscall)
+    kfree((void*)p->usyscall); 
+  p->usyscall=0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -203,6 +218,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  //会给虚拟地址USYSCALL映射物理地址p->usyscall
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)p->usyscall,PTE_R|PTE_U)<0){
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);//递归释放前先解除对TRAMPOLINE的映射
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -213,6 +236,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
